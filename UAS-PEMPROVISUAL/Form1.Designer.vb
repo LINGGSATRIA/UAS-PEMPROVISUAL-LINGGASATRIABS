9cb02b69128f33b426f0f5b963a411679330e8b3
ZENBOOK <ZENBOOK@LINGGA-SATRIA>
2023-02-20 02:56:04 +0700

    Add project files.

---
+++ UAS-PEMPROVISUAL/Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblWordCount = New System.Windows.Forms.Label()
+        Me.lblLineCount = New System.Windows.Forms.Label()
+        Me.lblCharactercount = New System.Windows.Forms.Label()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(1072, 62)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "PROGRAM MENGHITUNG JUMLAH KARAKTER, KATA, DAN BARIS DALAM FILE" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(57, 175)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(164, 24)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Jumlah Karakter"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(57, 245)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(127, 24)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Jumlah Kata"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(57, 314)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(128, 24)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Jumlah Baris"
+        '
+        'lblWordCount
+        '
+        Me.lblWordCount.AutoSize = True
+        Me.lblWordCount.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWordCount.Location = New System.Drawing.Point(419, 245)
+        Me.lblWordCount.Name = "lblWordCount"
+        Me.lblWordCount.Size = New System.Drawing.Size(0, 24)
+        Me.lblWordCount.TabIndex = 4
+        '
+        'lblLineCount
+        '
+        Me.lblLineCount.AutoSize = True
+        Me.lblLineCount.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLineCount.Location = New System.Drawing.Point(419, 314)
+        Me.lblLineCount.Name = "lblLineCount"
+        Me.lblLineCount.Size = New System.Drawing.Size(0, 24)
+        Me.lblLineCount.TabIndex = 5
+        '
+        'lblCharactercount
+        '
+        Me.lblCharactercount.AutoSize = True
+        Me.lblCharactercount.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCharactercount.Location = New System.Drawing.Point(419, 175)
+        Me.lblCharactercount.Name = "lblCharactercount"
+        Me.lblCharactercount.Size = New System.Drawing.Size(0, 24)
+        Me.lblCharactercount.TabIndex = 6
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOpen.Location = New System.Drawing.Point(539, 431)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(151, 35)
+        Me.btnOpen.TabIndex = 7
+        Me.btnOpen.Text = "Open"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(539, 493)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(151, 35)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(928, 583)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(151, 35)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1091, 643)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnOpen)
+        Me.Controls.Add(Me.lblCharactercount)
+        Me.Controls.Add(Me.lblLineCount)
+        Me.Controls.Add(Me.lblWordCount)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "PEMROGRAMAN VISUAL"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblWordCount As Label
+    Friend WithEvents lblLineCount As Label
+    Friend WithEvents lblCharactercount As Label
+    Friend WithEvents btnOpen As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ UAS-PEMPROVISUAL/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        ' Buka dialog open file dan baca isi file'
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "File Teks (*.docx)|*.docx"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim path As String = openFileDialog.FileName
+            Dim lines As String() = System.IO.File.ReadAllLines(path)
+            Dim characterCount As Integer = 0
+            Dim wordCount As Integer = 0
+
+            ' Hitung karakter, kata, dan baris'
+            For Each line In lines
+                characterCount += line.Length
+                Dim words As String() = line.Split(" ")
+                wordCount += words.Length
+            Next
+
+            ' Tampilkan hasil perhitungan'
+            lblCharactercount.Text = ":" & characterCount
+            lblWordCount.Text = ":" & wordCount
+            lblLineCount.Text = ":" & lines.Length
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Membersihkan kontrol'
+        lblCharacterCount.Text = String.Empty
+        lblWordCount.Text = String.Empty
+        lblLineCount.Text = String.Empty
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Keluar dari aplikasi'
+        Me.Close()
+    End Sub
+End Class
